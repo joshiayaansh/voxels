cbb395b5217b370a3e641c0248139ee555f0d534
joshiayaansh <joshiayaansh@gmail.com>
2026-03-06 17:50:53 -0600

    coom

--- app.py
+++ app.py
@@ -1050,4 +1050,4 @@ def handle_disconnect():
 init_db()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=80, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
